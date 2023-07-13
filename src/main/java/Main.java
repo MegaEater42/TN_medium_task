b8b96cf3f4137e46ec17413044ea309f6f44d6e7
MegaEater42 <ilyaandreev222003@gmail.ru>
2023-07-13 03:57:33 +0300

    Rewrite tree to int-type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Дерево_1:");
+        System.out.println("Дерево:");
         System.out.println(
                 """
-                           D
+                           5
                          /   \\
-                        C     E
+                        3     7
                          \\   /  \\
-                          B F    G
-                         /      / \\
-                        A      H   I
+                          2 6    18
+                         /      /
+                        1      17
+                              /
+                             16
                 """
         );
-        TreeNode<String> treeRootCase1 = new TreeNode<>("D");
-        treeRootCase1.setLeftChild(new TreeNode<>("C"));
-        treeRootCase1.getLeftChild().setRightChild(new TreeNode<>("B"));
-        treeRootCase1.getLeftChild().getRightChild().setLeftChild(new TreeNode<>("A"));
-        treeRootCase1.setRightChild(new TreeNode<>("E"));
-        treeRootCase1.getRightChild().setLeftChild(new TreeNode<>("F"));
-        treeRootCase1.getRightChild().setRightChild(new TreeNode<>("G"));
-        treeRootCase1.getRightChild().getRightChild().setLeftChild(new TreeNode<>("H"));
-        treeRootCase1.getRightChild().getRightChild().setRightChild(new TreeNode<>("I"));
-        System.out.println("Результат прямого обхода дерева_1 в глубину: " + TreeTraversal.preorderTraversal(treeRootCase1));
-        System.out.println("Результат обратного обхода дерева_1 в глубину: " + TreeTraversal.postorderTraversal(treeRootCase1));
-        System.out.println("Результат симметричного обхода дерева_1 в глубину: " + TreeTraversal.inorderTraversal(treeRootCase1));
-
-        System.out.println("Дерево_2:");
-        System.out.println(
-                """
-                             F
-                           /   \\
-                          B     G
-                         / \\     \\
-                        A   D     H
-                           / \\   /
-                          C   E I
-                """
-        );
-        TreeNode<String> treeRootCase2 = new TreeNode<>("F");
-        treeRootCase2.setLeftChild(new TreeNode<>("B"));
-        treeRootCase2.getLeftChild().setLeftChild(new TreeNode<>("A"));
-        treeRootCase2.getLeftChild().setRightChild(new TreeNode<>("D"));
-        treeRootCase2.getLeftChild().getRightChild().setLeftChild(new TreeNode<>("C"));
-        treeRootCase2.getLeftChild().getRightChild().setRightChild(new TreeNode<>("E"));
-        treeRootCase2.setRightChild(new TreeNode<>("G"));
-        treeRootCase2.getRightChild().setRightChild(new TreeNode<>("H"));
-        treeRootCase2.getRightChild().getRightChild().setLeftChild(new TreeNode<>("I"));
-
-        System.out.println("Результат прямого обхода дерева_2 в глубину: " + TreeTraversal.preorderTraversal(treeRootCase2));
-        System.out.println("Результат обратного обхода дерева_2 в глубину: " + TreeTraversal.postorderTraversal(treeRootCase2));
-        System.out.println("Результат симметричного обхода дерева_2 в глубину: " + TreeTraversal.inorderTraversal(treeRootCase2));
+        TreeNode<String> treeRootCase1 = new TreeNode<>("5");
+        treeRootCase1.setLeftChild(new TreeNode<>("3"));
+        treeRootCase1.getLeftChild().setRightChild(new TreeNode<>("2"));
+        treeRootCase1.getLeftChild().getRightChild().setLeftChild(new TreeNode<>("1"));
+        treeRootCase1.setRightChild(new TreeNode<>("7"));
+        treeRootCase1.getRightChild().setLeftChild(new TreeNode<>("6"));
+        treeRootCase1.getRightChild().setRightChild(new TreeNode<>("18"));
+        treeRootCase1.getRightChild().getRightChild().setLeftChild(new TreeNode<>("17"));
+        treeRootCase1.getRightChild().getRightChild().setLeftChild(new TreeNode<>("16"));
+        System.out.println("Результат прямого обхода дерева в глубину: " + TreeTraversal.preorderTraversal(treeRootCase1));
+        System.out.println("Результат обратного обхода дерева в глубину: " + TreeTraversal.postorderTraversal(treeRootCase1));
+        System.out.println("Результат симметричного обхода дерева в глубину: " + TreeTraversal.inorderTraversal(treeRootCase1));
     }
 }
